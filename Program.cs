8469ec0a3cceae9f6c06f46791c49a868859ade1
William Li <wul@microsoft.com>
2020-02-21 09:38:38 -0800

    Sample app

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace printingdot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+        }
+    }
+}
